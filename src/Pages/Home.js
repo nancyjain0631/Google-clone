b4143a22b3d7a32b6252ac8c42f4782a8336d3f4
Nancy Jain <nancyjain0631@gmail.com>
2021-03-21 19:50:53 +0530

    first

---
+++ src/Pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Home.css';
+function Home() {
+    return (
+        <div className="home">
+            <h1>Homepage</h1>
+
+            <div classNme="home_header">
+                <div className="home_headerLeft">
+                    {/* Link */}
+                    {/* Link */}
+                </div>
+                <div className="home_headerRight">
+                    {/* Link */}
+                    {/* Link */}
+                    {/* Icon */}
+                    {/* Avatar */}
+                </div>
+            </div>
+            <div classNme="home_body">
+                
+                </div>
+        </div>
+    )
+}
+
+export default Home
