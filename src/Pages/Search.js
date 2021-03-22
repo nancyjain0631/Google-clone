91917c61560b02d214087afca4869d732cf52422
Nancy Jain <nancyjain0631@gmail.com>
2021-03-22 14:16:08 +0530

    1

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Search.css';
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import {Button} from "@material-ui/core";
 function Search() {
     return (
         <div className='search'>
@@ -10,6 +11,11 @@ function Search() {
                     <input/>
                     <MicIcon className="search_micIcon"/>
                 
+            </div>
+            <div className="search_buttons">
+                <Button variant="outlined">Google Search</Button>
+                <Button variant="outlined">I'm Feeling Lucky</Button>
+
             </div>
         </div>
     )
