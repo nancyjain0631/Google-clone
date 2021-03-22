986fd91e9aa5c9d995a2dfb25f25951448810fdf
Nancy Jain <nancyjain0631@gmail.com>
2021-03-22 13:28:08 +0530

    header complete

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css';
 import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
 function Home() {
     return (
         <div className="home">
-            <h1>Homepage</h1>
+           
 
             <div className="home_header">
                 <div className="home_headerLeft">
@@ -15,8 +17,8 @@ function Home() {
                 <div className="home_headerRight">
                     <Link to='/gmail'>Gmail</Link>
                     <Link to='/images'>Images</Link>
-                    {/* Icon */}
-                    {/* Avatar */}
+                    <AppsIcon/>
+                    <Avatar/>
                 </div>
             </div>
             <div className="home_body">
