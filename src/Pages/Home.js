e2625e8162dcc587f9978edcbc758c60cefd0ef2
Nancy Jain <nancyjain0631@gmail.com>
2021-03-24 23:49:43 +0530

    6

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -27,7 +27,7 @@ function Home() {
                     src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
                     alt=""/>
                 <aiv className="home_inputContainer">
-                    <Search/>
+                    <Search hideButtons/>
                 </aiv>
             </div>
         </div>
