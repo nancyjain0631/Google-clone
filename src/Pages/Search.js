7b6d76c5d2a76a31090d7b9049c2f401d1672afc
Nancy Jain <nancyjain0631@gmail.com>
2021-03-24 23:19:24 +0530

    3

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css';
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import {Button} from "@material-ui/core";
 function Search() {
+    const [input, setInput] = useState("");
+    // const search =e =>{
+    //     e.preventDefault();
+    // }
     return (
         <div className='search'>
             <div className='search_input'>
                 <SearchIcon className="search_inputIcon"/>
-                    <input/>
+                    <input value={input} onChange={e => setInput(e.target.value)}/>
                     <MicIcon className="search_micIcon"/>
                 
             </div>
             <div className="search_buttons">
-                <Button variant="outlined">Google Search</Button>
+                <Button onClick={e => e.preventDefault()} variant="outlined">Google Search</Button>
                 <Button variant="outlined">I'm Feeling Lucky</Button>
 
             </div>
