ae9f8fa19eb70084b8f907e1a15f1b90501c7159
Nancy Jain <nancyjain0631@gmail.com>
2021-03-25 23:10:42 +0530

    api

--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import './SearchPage.css';
 import  { useStateValue } from "../StateProvider";
-
+import useGoogleSearch from '../useGoogleSearch';
+import { Link } from "react-router-dom";
+import Response from '../response';
+// https://developers.google.com/custom-search/v1/using_rest
+// https://cse.google.co.in/cse/create/new
 function SearchPage() {
     const [{term},dispatch] = useStateValue();
+// LIVE API CALL
+    // const { data }= useGoogleSearch(term);
+    const data = Response;
+    console.log(data);
     return (
         <div className='searchPage'>
             <div className="searchPage_header">
-                <h1>{term}</h1>
+                <Link to="/">
+                    <img
+                        className="searchPage_logo" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt=""
+                    />
+                </Link>
             </div>
             <div className="searchPage_results">
                 
--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -2,15 +2,21 @@ import { useState , useEffect } from 'react';
 import API_KEY from "./keys";
 const CONTEXT_KEY = "298fc13b0c3ce4335";
 
+// this is a custom hook
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
-    // this is a hook
+    
     useEffect(() => {
         const fetchData=async() => {
             fetch(
                 `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
             )
+            .then(response => response.json())
+            .then(result=>{
+                setData(result)
+            })
         }
+        fetchData();
     }, [term])
     // term is a data layer variable. whenever the term changes, we want this hook to be called
     return {data}
